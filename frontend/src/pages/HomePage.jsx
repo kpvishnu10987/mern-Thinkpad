c90342d036a16a57d914b6964cb9de61d211aa40
vishnu <kpvishnu10987@gmail.com>
2025-06-03 14:12:58 +0530

    prepared for the deployment

--- frontend/src/pages/HomePage.jsx
+++ frontend/src/pages/HomePage.jsx
@@ -50,7 +50,7 @@ useEffect(() => {
          {notes.length > 0 && !isRateLimited && (
           <div className="grid grid-cols-1 md:grid-cols-2 lg:grid-cols-3 gap-6">
             {notes.map((note) => (
-              <NoteCard key={note._id} note={note} setNotes={setNotes}  setNotes = {setNotes}/>
+              <NoteCard key={note._id} note={note} setNotes={setNotes}/>
             ))}
           </div>
         )}
